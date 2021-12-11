ec55706ef5fbc10e0e020aedb36ffbbf142961dc
edd26 <e.dmitruk@herts.ac.uk>
2021-12-11 15:18:53 +0000

    Added export for 2 spike correlation measures

--- src/SpikesCorrelations.jl
+++ src/SpikesCorrelations.jl
@@ -1,5 +1,13 @@
 module SpikesCorrelations
 
 # Write your package code here.
+export correlation_index, tailing_coefficient
+
 
 end
+
+# TODO:
+# - set up documentation
+# - add tests
+# - refactor vp distance, so that it is similar to other functions
+# - check out how to write a function that takes other functions as argument, just like in distances library
