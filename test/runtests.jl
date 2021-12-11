50c6e20b789d47cc8a65b76cd9466dcafe475fd7
edd26 <e.dmitruk@herts.ac.uk>
2021-12-11 15:18:31 +0000

    Added test empty test files for existing measures

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "SpikesCorrelations.jl" begin
     # Write your tests here.
+    include("test_CorrelationIndex.jl")
+    include("test_SpikeTrain.jl")
+    include("test_TilingCoefficient.jl")
+    include("test_VPDistance.jl")
 end
